64394ea0baa2dc93c5dd11f01ca0c9cf6495a367
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2024-11-30 07:49:36 -0300

    making http requests using curlpp

--- rest/libcurl_http_request/request_curlpp.cpp
+++ rest/libcurl_http_request/request_curlpp.cpp
@@ -1,10 +1,23 @@
 #include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
 #include <curlpp/cURLpp.hpp>
 #include <iostream>
+#include <strstream>
 
 int main(int argc, char **argv)
 {
-    cURLpp::initialize();
+    curlpp::Easy request;
+    curlpp::options::Url url("https://example.com");
+    request.setOpt(url);
+
+    std::strstream os;
+    curlpp::options::WriteStream writeStream(&os);
+    request.setOpt(writeStream);
+    request.perform();
+
+    std::string data;
+
+    os >> std::cout.rdbuf();
 
     return 0;
 }
